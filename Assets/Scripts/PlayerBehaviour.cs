dbe5687ec36105a8bb3f5647d173096860b8c7ba
Nadel1 <nataliapio@yahoo.de>
2020-09-28 18:11:11 +0200

    added basic jump

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -9,13 +9,22 @@ public class PlayerBehaviour : MonoBehaviour
     public float walkSpeed;
     public float runSpeed;
     public float rotSpeed;
+    public float jumpSpeed = 20;
+    public float jumpHeight = 10;
 
     private bool running = false;
+    private bool jumping;
+    private bool grounded;
     private Vector3 moveDir=new Vector3(0,0,0);
+    private Vector3 groundPos;
+    private int gravity;
+
+    private float secondsLeft = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        gravity = grounded ? 0 : 1;
     }
 
     // Update is called once per frame
@@ -30,9 +39,49 @@ public class PlayerBehaviour : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
             moveDir += transform.right;
 
-        rb.MovePosition(transform.position + walkSpeed * moveDir * Time.fixedDeltaTime);
+  
 
+        rb.MovePosition(transform.position + walkSpeed * moveDir * Time.fixedDeltaTime);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumping = !jumping;
+
+        }
+
+        if (jumping)
+        {
+            while (secondsLeft > 0)
+            {
+                secondsLeft -= Time.deltaTime;
+                rb.AddForce(new Vector3(0, 7, 0), ForceMode.Acceleration);
+            }
+            jumping = !jumping;
+            secondsLeft = 0.5f;
+        }
         rb.rotation = Quaternion.Euler(rb.rotation.eulerAngles + new Vector3(0f, rotSpeed * Input.GetAxis("Mouse X"), 0f));
         moveDir = Vector3.zero;
+        groundPos = new Vector3(transform.position.x, 0f, transform.position.z);
+
+    }
+
+   
+
+    IEnumerator Jump()
+    {
+        while (true)
+        {
+            if (transform.position.y >= jumpHeight)
+                jumping = false;
+            if (jumping)
+                rb.MovePosition(transform.up * jumpSpeed * Time.smoothDeltaTime);
+            else if (!jumping)
+            {
+                rb.MovePosition( Vector3.Lerp(transform.position, groundPos, 10* Time.smoothDeltaTime));
+                if (transform.position == groundPos)
+                    StopAllCoroutines();
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
