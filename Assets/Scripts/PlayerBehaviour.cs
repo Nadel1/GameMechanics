01e31d07ba97c677fa7717a6e3b2a983b5577e45
Nadel1 <nataliapio@yahoo.de>
2020-09-28 16:45:17 +0200

    basic movement

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    private Rigidbody rb;
+
+    public float walkSpeed;
+    public float runSpeed;
+    public float rotSpeed;
+
+    private bool running = false;
+    private Vector3 moveDir=new Vector3(0,0,0);
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+            moveDir += transform.forward;
+        if (Input.GetKey(KeyCode.S))
+            moveDir -= transform.forward;
+        if (Input.GetKey(KeyCode.A))
+            moveDir -= transform.right;
+        if (Input.GetKey(KeyCode.D))
+            moveDir += transform.right;
+
+        rb.MovePosition(transform.position + walkSpeed * moveDir * Time.fixedDeltaTime);
+
+        rb.rotation = Quaternion.Euler(rb.rotation.eulerAngles + new Vector3(0f, rotSpeed * Input.GetAxis("Mouse X"), 0f));
+        moveDir = Vector3.zero;
     }
 }
