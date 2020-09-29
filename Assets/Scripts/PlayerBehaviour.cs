fd137a01698eb5b85780474b10227c93c73e5f7e
Nadel1 <nataliapio@yahoo.de>
2020-09-29 16:26:05 +0200

    added states

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -9,7 +9,7 @@ public class PlayerBehaviour : MonoBehaviour
     [SerializeField]
     [Range(1,50)]
     [Tooltip("Speed at which the character moves while walking")]
-    private float walkSpeed=10;
+    private float walkSpeed=5;
 
     [SerializeField]
     [Range(1, 50)]
@@ -19,33 +19,46 @@ public class PlayerBehaviour : MonoBehaviour
     [SerializeField]
     [Range(1, 50)]
     [Tooltip("Speed at which the character rotates")]
-    public float rotSpeed=1;
+    private float rotSpeed=1;
 
     [SerializeField]
     [Range(1, 50)]
     [Tooltip("Speed at which the character jumps")]
-    public float jumpSpeed = 20;
+    private float jumpSpeed = 20;
 
+    [SerializeField]
+    private enum State { Idle,Walking,Running,Jumping};
+
+    [SerializeField]
+    private State currentState = State.Idle;
 
     //private variables
     private Rigidbody rb;
-    private bool running = false;
     private bool jumping;
-    private bool grounded;
     private Vector3 moveDir=new Vector3(0,0,0);
     private Vector3 groundPos;
-    private int gravity;
+
+    private float speed;
 
     private float secondsLeft = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        gravity = grounded ? 0 : 1;
+
+        
     }
 
     // Update is called once per frame
     void FixedUpdate()
+    {
+        CheckInput();
+        Jumping();
+        Turn();
+    }
+
+
+    private void CheckInput()
     {
         if (Input.GetKey(KeyCode.W))
             moveDir += transform.forward;
@@ -56,9 +69,23 @@ public class PlayerBehaviour : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
             moveDir += transform.right;
 
-  
+        if (Input.GetKey(KeyCode.LeftShift)&&speed==walkSpeed)
+            currentState = State.Running;
+        
+        if (Input.GetKey(KeyCode.LeftShift) && speed == runSpeed)
+            currentState = State.Walking;
+        
 
-        rb.MovePosition(transform.position + walkSpeed * moveDir * Time.fixedDeltaTime);
+        speed = currentState.Equals(State.Running) ? runSpeed : walkSpeed;
+        Debug.Log(speed);
+        rb.MovePosition(transform.position + speed* moveDir * Time.fixedDeltaTime);
+        if (!moveDir.Equals(Vector3.zero) && currentState != State.Idle && currentState != State.Running)
+            currentState = State.Walking;
+        moveDir = Vector3.zero;
+    }
+
+    private void Jumping()
+    {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             jumping = !jumping;
@@ -70,17 +97,17 @@ public class PlayerBehaviour : MonoBehaviour
             while (secondsLeft > 0)
             {
                 secondsLeft -= Time.deltaTime;
-                rb.AddForce(new Vector3(0,2,0)*jumpSpeed, ForceMode.Acceleration);
+                rb.AddForce(new Vector3(0, 2, 0) * jumpSpeed, ForceMode.Acceleration);
             }
             jumping = !jumping;
             secondsLeft = 0.5f;
         }
-        rb.rotation = Quaternion.Euler(rb.rotation.eulerAngles + new Vector3(0f, rotSpeed * Input.GetAxis("Mouse X"), 0f));
-        moveDir = Vector3.zero;
         groundPos = new Vector3(transform.position.x, 0f, transform.position.z);
-
     }
 
-   
+    private void Turn()
+    {
+        rb.rotation = Quaternion.Euler(rb.rotation.eulerAngles + new Vector3(0f, rotSpeed * Input.GetAxis("Mouse X"), 0f));
+    }
 
 }
