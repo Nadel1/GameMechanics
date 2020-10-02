57bfbf05e0228c6d7ea104d500cf75ce143a9a00
Nadel1 <nataliapio@yahoo.de>
2020-10-02 19:50:46 +0200

    aiming works

--- Assets/Scripts/CharacterAiming.cs
+++ Assets/Scripts/CharacterAiming.cs
@@ -1,11 +1,16 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+
 
 public class CharacterAiming : MonoBehaviour
 {
     public float turnSpeed = 15;
     private Camera mainCamera;
+
+
     // Start is called before the first frame update
     void Start()
     {
